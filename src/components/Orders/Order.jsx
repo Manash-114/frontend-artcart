7dd8531c289b759c78b1e847b059c9a090c18d7c
beihrazi <beihrazi@gmail.com>
2024-04-24 22:36:04 +0530

    a

--- src/components/Orders/Order.jsx
+++ src/components/Orders/Order.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
@@ -9,13 +9,44 @@ import { Button, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 const Order = () => {
+
+  const [selectedValue, setSelectedValue] = useState("processing")
+
+  const [data, setData] = useState([]);
+
+
+  //deliver
+  const deliverOrders = useSelector((state) => state.customer.allDeliveredOrders);
+  console.log("deliver value:", deliverOrders);
+
+  //undeliver
+  const undeliverOrders = useSelector((state) => state.customer.allNotDeliveredOrders);
+
+  // console.log("Undeliver value:", UndeliverOrders);
+  useEffect(() => {
+    if (selectedValue === "processing") {
+      setData(deliverOrders);
+    } else {
+      setData(undeliverOrders);
+    }
+  }, [selectedValue, deliverOrders, undeliverOrders]);
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value)
+    if (selectedValue === "processing") {
+      setData(deliverOrders)
+    } else {
+      setData(UndeliverOrders)
+    }
+  };
   return (
     <Wrapper>
       <Container>
         <Left>
-          <h2>Filters</h2>
+
           <FormControl>
             <FormLabel id="demo-radio-buttons-group-label">
               Order Status
@@ -24,16 +55,17 @@ const Order = () => {
               aria-labelledby="demo-radio-buttons-group-label"
               defaultValue="processing"
               name="radio-buttons-group"
+              onChange={handleChange}
             >
               <FormControlLabel
                 value="processing"
                 control={<Radio />}
-                label="On the way"
+                label="Delivered"
               />
               <FormControlLabel
                 value="delivered"
                 control={<Radio />}
-                label="Delivered"
+                label="On the way"
               />
             </RadioGroup>
           </FormControl>
@@ -63,20 +95,66 @@ const Order = () => {
           </div> */}
 
           {/* order-product */}
+          {
+            data.length > 0 ? (
+              data.map((order) => (
+                <div className="product-container">
+                  <div className="image">
+                    <img
+                      src={order.productBelongsToOrder.products.productImages[0].name}
+                      alt={order.productBelongsToOrder.products}
+                    />
+                  </div>
+                  <div className="title">
+                    <p className="pname">
+                      {order.productBelongsToOrder.products.name.length > 20
+                        ? order.productBelongsToOrder.products.name.slice(0, 30) +
+                        ".."
+                        : order.productBelongsToOrder.products.name}
+                    </p>
+                    <p className="orderId">
+                      OrderId: <span className="OId">{order.orderId}</span>
+                    </p>
+                    {order.productBelongsToOrder.courierName !== "NULL" && (
+                      <p className="courier">
+                        Courier Name: <span className="cId">{order.productBelongsToOrder.courierName}</span>
+                      </p>
+                    )}
 
-          <div className="product-container">
-            <div className="image">
-              <img src="/images/scenery3.jpg" alt="title" />
-            </div>
-            <div className="title">
-              <p>Lakmé Sunscreen - SPF 50 .. </p>
-              <p>OrderId: <span className="OId">id12345</span></p>
-            </div>
-            <div className="status">
-              <p>Status</p>
-              <span>Your order has been packed</span>
-            </div>
-          </div>
+                  </div>
+                  <div className="status">
+                    <p>Status</p>
+                    <span
+                      className="statusId"
+                      style={{
+                        backgroundColor:
+                          order.productBelongsToOrder.deliveryStatus === "SHIPPED"
+                            ? "#0edb1c"
+                            : "#0caae3",
+                      }}
+                    >
+                      {order.productBelongsToOrder.deliveryStatus === "SHIPPED"
+                        ? "Delivered"
+                        : "Pending"}
+                    </span>
+
+
+                    <span className="date">
+                      Order Date: {new Date(order.orderDate).toLocaleDateString('en-IN', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric'
+                      })}
+                    </span>
+
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>No products found</p>
+            )
+          }
+
         </Right>
       </Container>
     </Wrapper>
@@ -98,16 +176,22 @@ const Container = styled.section`
   min-height: 50vh;
 `;
 const Left = styled.div`
-  border: 1px solid black;
-  flex: 0.3;
+ border: 1px solid #03112099;
+  border-radius: 20px;
+  flex: 0.2;
   padding-left: 2rem;
-
+  display: flex;
+  justify-content: flex-start;
   #demo-radio-buttons-group-label {
-    margin-bottom: 1rem;
+    font-size: 1.2rem;
+    margin: 2rem auto;
+    color: black;
   }
+  
 `;
 const Right = styled.div`
-  border: 1px solid black;
+  border: 1px solid #2003034b;
+  border-radius: 10px;
   flex: .8;
   padding: 1rem;
 
@@ -115,7 +199,8 @@ const Right = styled.div`
   }
 
   .product-container {
-    border: 1px solid black;
+    border: 1px solid #03112099;
+  border-radius: 10px;
     display: flex;
     margin-top: 1.5rem;
     padding: 10px;
@@ -133,8 +218,8 @@ const Right = styled.div`
   }
   .title {
     flex: 0.8;
-    border: 1px solid black;
-    padding: 1rem;
+    /* border: 1px solid black; */
+    padding: .5rem 1rem;
   }
   .price {
     flex: 0.3;
@@ -146,15 +231,49 @@ const Right = styled.div`
   }
   .status {
     flex: 0.7;
+    border-left: 1px solid #079724;
     /* border: 1px solid black; */
     display: flex;
     justify-content: flex-start;
     align-items: flex-start;
     flex-direction: column;
-    padding-left: 10px;
+    padding-left: 15px;
 
     p {
       font-weight: 550;
     }
+  }
+  .pname{
+    font-weight: 550;
+  }
+  .courier{
+    font-size: .8rem;
+    padding-top: 1.8rem;
+  }
+  .cId{
+    font-weight: 600;
+  }
+  .orderId{
+    font-size: .8rem;
+    padding-top: 10px;
+  }
+  .OId{
+    font-weight: 550;
+  }
+  .statusId{
+    background-color: #09c63c;
+    margin-top: .5rem;
+    color: white;
+    /* border: 1px solid black; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 120px;
+    height: 25px;
+    border-radius: 10px;
+  }
+  .date{
+    margin-top: 1rem;
+    font-size: .9rem;
   }
 `;
