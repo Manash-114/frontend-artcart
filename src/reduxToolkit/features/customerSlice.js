12d8ff85bb92e6bfdebff928e476b967125ddcc6
Manash Jyoti Handique <m@gmail.com>
2024-04-24 17:27:30 +0530

    get all undeliverd order

--- src/reduxToolkit/features/customerSlice.js
+++ src/reduxToolkit/features/customerSlice.js
@@ -3,14 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const customerSlice = createSlice({
   name: "customerSlice",
   initialState: {
+    allNotDeliveredorders: [],
     allorders: [],
   },
   reducers: {
     updateAllOrders: (state, action) => {
       state.allorders = action.payload;
     },
+    updateAllNotDeliverdOrders: (state, action) => {
+      state.allNotDeliveredorders = action.payload;
+    },
   },
 });
 
-export const { updateAllOrders } = customerSlice.actions;
+export const { updateAllOrders, updateAllNotDeliverdOrders } =
+  customerSlice.actions;
 export default customerSlice.reducer;
