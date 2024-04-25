3e4904ee6cd32acae80976fe854950ddf9bebc6e
Manash Jyoti Handique <m@gmail.com>
2024-04-25 13:08:54 +0530

    change url

--- src/apiCalls/common-db.js
+++ src/apiCalls/common-db.js
@@ -1,3 +1,6 @@
 export const BASE_URL = "http://65.0.169.228:7002";
-export const BASE_URL_LOCAL = "http://192.168.137.82:7002";
+// export const BASE_URL_LOCAL = "http://192.168.137.82:7002";
+// export const BASE_URL_LOCAL = "http://192.168.137.82:7002";
+
 // export const BASE_URL_LOCAL = "http://artcartapi.manash.online";
+export const BASE_URL_LOCAL = "http://artcartapi.manash.online";
