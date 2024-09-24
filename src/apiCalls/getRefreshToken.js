226d335c91d6163a2666574ca39b6ce3841f769c
Manash-114 <manash0119@gmail.com>
2024-09-24 23:05:51 +0530

    update refresh token api

--- src/apiCalls/getRefreshToken.js
+++ src/apiCalls/getRefreshToken.js
@@ -1,29 +1,20 @@
 import axios from "./axios";
 // Function to refresh the access token
-const getRefreshToken = async (authState, updateCredentials, handlelogOut) => {
+const getRefreshToken = async (authState, updateCredentials) => {
   try {
     const response = await axios.get("/auth/refresh-token", {
       withCredentials: true,
     });
-
     const newAccessToken = response.data.accessToken;
     // Use a callback function to update the credentials in the Redux store
     updateCredentials({
       ...authState,
       token: newAccessToken,
     });
+
     return newAccessToken;
   } catch (error) {
-    // Check if the error is related to the refresh token expiry or invalidity
-    if (error.response.status === 401) {
-      // 401 typically indicates that the refresh token has expired or is invalid
-      // Call the logout function to log out the user
-      console.log(`error with code ${error?.status}`);
-      handlelogOut();
-    } else {
-      // Handle other errors (e.g., network errors)
-      throw error;
-    }
+    throw error;
   }
 };
 
