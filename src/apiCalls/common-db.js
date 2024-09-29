e3746d5a40a446d3016eebd4b0867f5a59e0a737
Manash-114 <manash0119@gmail.com>
2024-09-29 17:28:11 +0530

    update url

--- src/apiCalls/common-db.js
+++ src/apiCalls/common-db.js
@@ -1,8 +1,2 @@
-// export const BASE_URL = "http://65.0.169.228:7002";
-// export const BASE_URL_LOCAL = "http://192.168.137.82:7002";
-// export const BASE_URL_LOCAL = "http://192.168.137.82:7002";
-// https://backend-artcart-yv14.onrender.com/
-// export const BASE_URL_LOCAL = "http://artcartapi.manash.online";
-// export const BASE_URL_LOCAL = "https://backend-artcart-yv14.onrender.com";
-export const BASE_URL_LOCAL = "http://localhost:7002";
-// export const BASE_URL_LOCAL = "http://10.68.1.32:7002";
+export const BASE_URL_LOCAL = "https://backend-artcart-yv14.onrender.com";
+// export const BASE_URL_LOCAL = "http://localhost:7002";
